4e244248c3cd0167806ccc6688ce3c1105a2bd7f
Andy Collins <askingalot@gmail.com>
2017-01-15 16:05:22 -0600

    Adding Util class (every project needs one!)

---
+++ Lillian/Lib/Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace Lillian.Lib
+{
+    public static class Utils
+    {
+        public static string ToCamelCase(this string source)
+        {
+            return char.ToLower(source[0]) + source.Substring(1);
+        }
+    }
+}
